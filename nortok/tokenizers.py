e3d7fa3a528dcc009aa3f95a8d2965e8261104e8
ErlendA <erlend.aune@amedia.no>
2017-04-28 09:28:55 +0200

    Refactoring. Saving works for simple tokenizers.

--- nortok/tokenizers.py
+++ nortok/tokenizers.py
@@ -6,6 +6,8 @@ import numpy as np
 from collections import defaultdict,Counter
 import logging as logger
 from nortok.stopwords import get_norwegian_stopwords
+import pickle
+import gzip
 
 def dd_def():
     return 0
@@ -32,16 +34,12 @@ def _fit_tokenizer(texts,tokfunc,max_length=None):
                 ind2word[maxInd]=c
     return word2ind,ind2word,wordcount
 
-def _max_word_vocab(word2ind,ind2word,wordcount,max_words=None):
-    def skey(x):
-        return x[1]
+def _max_word_vocab(word2ind,ind2word,wcs,max_words=None):
     if max_words is None:
         return word2ind,ind2word
     if len(word2ind)<max_words:
         logger.info('len(word2ind)<=max_words:{0}'.format(max_words))
         return word2ind,ind2word
-    wcs=list(wordcount.items())
-    wcs.sort(key=skey,reverse=True)
     w2i=defaultdict(dd_def)
     i2w={}
     wordInd=1
@@ -65,18 +63,76 @@ def _texts_to_seqs(texts,tokfunc,word2ind,max_len,n_texts=None):
         seqs[iii,:ml]=[word2ind[tok] for tok in toks]
     return seqs
 
-def texts_to_seqs_var(texts,tokfunc,word2ind,max_len):
+def texts_to_seqs_var(texts,tokfunc,word2ind,max_len=None):
     for txt in texts:
-        toks=tokfunc(txt,max_len)
+        toks=tokfunc(txt,max_length=max_len)
         yield [word2ind[tok] for tok in toks]
 
-
-class WordTokenizer(TweetTokenizer):
-    def __init__(self,word2ind=None,use_stopwords=False,use_stemmer=False,max_words=None,**kwargs):
-        super(WordTokenizer, self).__init__(**kwargs)
+class BaseTokenizer(object):
+    def __init__(self,word2ind=None,max_words=None,min_freq=2,**kwargs):
         if word2ind is not None:
             self.document_count=1
-            self.word2ind=defaultdict(lambda:0,word2ind)
+            self.word2ind=defaultdict(dd_def,word2ind)
+        self.min_freq=min_freq
+
+    def tokenize(self,text,max_length=None):
+        toks=text.split()
+        if max_length:
+            toks=toks[:max_length]
+        return toks
+
+    def texts_to_sequences(self,texts,max_len,n_texts=None):
+        seqs=_texts_to_seqs(texts,self.tokenize,self.word2ind,max_len,n_texts)
+        return seqs
+
+    def var_length_texts_to_sequences(self,texts):
+        tt=texts_to_seqs_var(texts,self.tokenize,self.word2ind)
+        for seq in tt:
+            yield seq
+
+    def fit_tokenizer(self,texts,max_length,max_words=None):
+        word2ind,ind2word,wordcount=_fit_tokenizer(texts,self.tokenize,max_length=max_length)
+        wordcount=dict((q,r) for q,r in wordcount.items() if r>=self.min_freq)
+
+        def skey(x):
+            return x[1]
+        wcs=list(wordcount.items())
+        wcs.sort(key=skey,reverse=True)
+
+        self.wordcount=wcs
+        self.word2ind,self.ind2word=_max_word_vocab(word2ind,ind2word,self.wordcount,max_words)
+        self.max_words=max_words
+
+    def prune_vocab(self,max_words=None):
+        if max_words>=self.max_words:
+            raise ValueError("Can't prune with larger vocabulary.")
+        self.word2ind,self.ind2word=_max_word_vocab(self.word2ind,self.ind2word,self.wordcount,max_words)
+        self.max_words=max_words
+
+    def save_tokenizer(self,savepath,extraobjs=None):
+        w2i=list(self.word2ind.items())
+        i2w=list(self.ind2word.items())
+        outdict={'word2ind':w2i,'ind2word':i2w,'max_words':self.max_words}
+        if extraobjs:
+            outdict.update(extraobjs)
+        with gzip.open(savepath,'wb') as ff:
+            pickle.dump(outdict,ff)
+
+    @staticmethod
+    def load_tokenizer(savepath):
+        with gzip.open(savepath,'rb') as ff:
+            indict=pickle.load(ff)
+        indict['word2ind']=defaultdict(dd_def,indict['word2ind'])
+        indict['ind2word']=dict(indict['ind2word'])
+        tok=WordTokenizer(indict)
+        for k,v in indict.items():
+            setattr(tok,k,v)
+        return tok
+
+class WordTokenizer(BaseTokenizer):
+    def __init__(self,word2ind=None,use_stopwords=False,use_stemmer=False,max_words=None,**kwargs):
+        super(WordTokenizer, self).__init__(**kwargs)
+        self.tweetok=TweetTokenizer(**kwargs)
         if use_stopwords:
             if isinstance(use_stopwords,set):
                 self.stopwords=use_stopwords
@@ -89,8 +145,13 @@ class WordTokenizer(TweetTokenizer):
         if use_stemmer==True:
             self.stemmer=SnowballStemmer('norwegian')
 
+    def def_eobjs(self):
+        return {'use_stemmer':self.use_stemmer,'stopwords':self.stopwords}
+
     def tokenize(self,text,max_length=512):
-        toks=super(WordTokenizer,self).tokenize(text)[:max_length]
+        toks=self.tweetok.tokenize(text)
+        if max_length:
+            toks=toks[:max_length]
         if self.stopwords and (not self.use_stemmer):
             toks=[t for t in toks if t not in self.stopwords]
         elif self.stopwords and self.use_stemmer:
@@ -99,45 +160,24 @@ class WordTokenizer(TweetTokenizer):
             toks=[self.stemmer.stem(t) for t in toks]
         return toks
 
-    def texts_to_sequences(self,texts,max_len,n_texts=None):
-        seqs=_texts_to_seqs(texts,self.tokenize,self.word2ind,max_len,n_texts)
-        return seqs
-
-    def fit_tokenizer(self,texts,max_length,max_words=None):
-        word2ind,ind2word,wordcount=_fit_tokenizer(texts,self.tokenize,max_length=max_length)
-        self.word2ind,self.ind2word=_max_word_vocab(word2ind,ind2word,wordcount,max_words)
-        self.max_words=max_words
+    def save_tokenizer(self,savepath):
+        eobjs=self.def_eobjs()
+        super(WordTokenizer, self).save_tokenizer(savepath=savepath,extraobjs=eobjs)
 
 
-class RawCharTokenizer(object):
+class RawCharTokenizer(BaseTokenizer):
     def __init__(self,word2ind=None,max_words=None):
         self.max_words=max_words
-        if word2ind is not None:
-            self.document_count=1
-            self.word2ind=defaultdict(dd_def,word2ind)
-        #self.max_words=max_words
+
+    def tokenize(self,text,max_length=None):
+        toks=list(text.lower())
+        if max_length:
+            toks=toks[:max_length]
+        return toks
 
 
-    def tokenize(self,text,max_length=2048):
-        return list(text.lower())[:max_length]
-
-    def texts_to_sequences(self,texts,max_len,n_texts=None):
-        seqs=_texts_to_seqs(texts,RawCharTokenizer.tokenize,self.word2ind,max_len,n_texts)
-        return seqs
-
-    def fit_tokenizer(self,texts,max_length,max_words=None):
-        word2ind,ind2word,wordcount=_fit_tokenizer(texts,self.tokenize,max_length=max_length)
-        self.word2ind,self.ind2word=_max_word_vocab(word2ind,ind2word,wordcount,max_words)
-        self.max_words=max_words
-
-
-
-
-class HierarchicalTokenizer(object):
+class HierarchicalTokenizer(BaseTokenizer):
     def __init__(self,word2ind=None,max_words=None):
-        if word2ind is not None:
-            self.document_count=1
-            self.word2ind=defaultdict(dd_def,word2ind)
         self.max_words=max_words
         self.wordtok=WordTokenizer()
         self.chartok=RawCharTokenizer(max_words=max_words)
---
+++ testscript.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nortok
+import argparse
+import numpy as np
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Tool to test the tokenizer')
+    parser.add_argument("--outfilepath",\
+        dest='ofpath', help="File to write tokenizer")
+    parser.add_argument("--infilepath",\
+        dest='ifpath',help="File with texts, newline per doc")
+    args=parser.parse_args()
+
+    tok=nortok.WordTokenizer(preserve_case=False)
+    with open(args.ifpath) as ff:
+        txts=ff.readlines()
+
+    tok.fit_tokenizer(txts,1000,max_words=1000)
+    tok.save_tokenizer(args.ofpath)
+
+    tok2=nortok.WordTokenizer.load_tokenizer(args.ofpath)
+
+    seq1=tok.texts_to_sequences(txts,200)
+    seq2=tok2.texts_to_sequences(txts,200)
+
+    print('Sequences equal after loading tokenizer:',not np.all(seq1-seq2))
+
+    tok.prune_vocab(max_words=900)
+
+    seq3=tok.texts_to_sequences(txts,200)
+
+    cols,rows=np.where((seq1-seq3)!=0)
+
+    print('After pruning, {0} \
+entries are different from original. Original vocab size: {1}, \
+pruned vocab size: {2}'.format(len(cols),tok2.max_words,tok.max_words))
